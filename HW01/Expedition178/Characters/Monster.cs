d69e514238130fd5a35d531f36652cd3aa0b5f0c
Filip Kudera <xkudera@fi.muni.cz>
2026-03-04 18:14:56 +0100

    feat: implement constructor

--- HW01/Expedition178/Characters/Monster.cs
+++ HW01/Expedition178/Characters/Monster.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Expedition178.GameMechanics;
+
 namespace Expedition178.Characters
 {
     internal class Monster : Character
     {
+        public Monster(string name, CharacterType charType, AttackType attackType) : base(name, charType, attackType) { }
     }
 }
