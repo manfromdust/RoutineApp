9e3a927f6a7383f87fd7ae8f7d35fe5f72dd2272
Filip Kudera <xkudera@fi.muni.cz>
2026-03-04 18:14:38 +0100

    feat: implement constructor and override ToString method

--- HW01/Expedition178/Characters/Adventurer.cs
+++ HW01/Expedition178/Characters/Adventurer.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Expedition178.GameMechanics;
+
 namespace Expedition178.Characters
 {
     internal class Adventurer : Character
     {
-    }
+        private int Level { get; set; }
+
+        public Adventurer(string name, CharacterType charType, AttackType attackType) : base(name, charType, attackType)
+        {
+            Level = 1;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Level: {Level}";
+        }
 }
