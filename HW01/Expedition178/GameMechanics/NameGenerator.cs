cfb755970a439024d3c287d4e0c70f34903ab90c
Filip Kudera <xkudera@fi.muni.cz>
2026-03-04 18:53:37 +0100

    feat: add class through which we can get random name

---
+++ HW01/Expedition178/GameMechanics/NameGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expedition178.GameMechanics
+{
+    internal static class NameGenerator
+    {
+        private static readonly string path = "names.txt";
+
+        public static string GenerateName()
+        {
+            var names = File.ReadAllLines(path);
+            var random = new Random();
+            return names[random.Next(names.Length)];
+        }
+    }
+}
