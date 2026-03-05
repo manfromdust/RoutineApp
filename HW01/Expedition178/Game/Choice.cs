48ca6ae15326efb6592d833abfa1907a90632847
Filip Kudera <xkudera@fi.muni.cz>
2026-03-05 19:23:32 +0100

    feat: add enum class for input choices

---
+++ HW01/Expedition178/Game/Choice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expedition178.Game
+{
+    internal enum Choice
+    {
+        Check,
+        Fight,
+        Info,
+        Help,
+        Sort,
+        Quit
+    }
+}
