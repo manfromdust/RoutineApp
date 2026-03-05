71ecedbc32df3810cd44d2a62efe0c04a5acfa74
Filip Kudera <xkudera@fi.muni.cz>
2026-03-05 18:49:17 +0100

    refactor: change properties to fields

--- HW01/Expedition178/Characters/Character.cs
+++ HW01/Expedition178/Characters/Character.cs
@@ -8,37 +8,37 @@ namespace Expedition178.Characters
 {
     internal abstract class Character
     {
-        public string Name { protected get; init; }
-        protected int Attack { get; set; }
-        protected int Life { get; set; }
-        protected int Speed { get; set; }
-        protected CharacterType CharType { get; init; }
-        protected AttackType AttackType { get; init; }
+        public readonly string name;
+        protected int attack;
+        protected int life;
+        protected int speed;
+        protected readonly CharacterType charType;
+        protected readonly AttackType attackType;
 
         public Character(string name, CharacterType charType, AttackType attackType)
         {
-            Name = name;
-            AttackType = attackType;
-            CharType = charType;
+            this.name = name;
+            this.attackType = attackType;
+            this.charType = charType;
 
             switch (charType)
             {
                 case CharacterType.Tank:
-                    Attack = 15;
-                    Life = 120;
-                    Speed = 5;
+                    attack = 15;
+                    life = 120;
+                    speed = 5;
                     break;
                 case CharacterType.Dps:
-                    Attack = 30;
-                    Life = 70;
-                    Speed = 10;
+                    attack = 30;
+                    life = 70;
+                    speed = 10;
                     break;
             }
         }
 
         public override string ToString()
         {
-            return $"{Name} ({CharType}, {AttackType}) - Attack: {Attack}, Life: {Life}, Speed: {Speed}";
+            return $"{name} ({charType}, {attackType}) - Attack: {attack}, Life: {life}, Speed: {speed}";
         }
     }
 }
