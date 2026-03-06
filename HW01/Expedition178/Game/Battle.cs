438e1f17db7cd8a6c9a5d2dc52ad9ac7aa1c46a7
Filip Kudera <xkudera@fi.muni.cz>
2026-03-06 20:50:37 +0100

    feat: implement constructor for Battle class and Field Monsters

--- HW01/Expedition178/Game/Battle.cs
+++ HW01/Expedition178/Game/Battle.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Expedition178.Characters;
+using Expedition178.GameMechanics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Expedition178.Game
 {
-    internal class Battle
+    public class Battle : IBattle
     {
+        public Monster[] Monsters { get; init; }
+
+        public Battle(int wave)
+        {
+            Monsters = CharacterGenerator.GenerateMonsters(wave);
+        }
     }
 }
