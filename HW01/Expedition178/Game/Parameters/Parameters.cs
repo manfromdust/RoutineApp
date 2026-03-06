585e5a4a96d4fa1d188daa0ad36f1154103c3d4c
Filip Kudera <xkudera@fi.muni.cz>
2026-03-06 20:27:35 +0100

    feat: implement static class containing parameters for the game

---
+++ HW01/Expedition178/Game/Parameters/Parameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expedition178.Game.Parameters
+{
+    public static class Parameters
+    {
+        public const int MaxAdventurers = 3;
+        public const int MaxWaves = 3;
+        public const int MaxMonsters = 3;
+
+        public const int MaxDpsAttack = 30;
+        public const int MinDpsAttack = 20;
+        public const int MaxDpsLife = 70;
+        public const int MinDpsLife = 50;
+        public const int MaxDpsSpeed = 11;
+        public const int MinDpsSpeed = 7;
+
+        public const int MaxTankAttack = 15;
+        public const int MinTankAttack = 10;
+        public const int MaxTankLife = 120;
+        public const int MinTankLife = 90;
+        public const int MaxTankSpeed = 7;
+        public const int MinTankSpeed = 4;
+    }
+}
