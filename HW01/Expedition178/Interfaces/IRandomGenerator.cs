65333bc32c62f426a8334df40361c78d1f86c293
Filip Kudera <xkudera@fi.muni.cz>
2026-03-07 23:08:32 +0100

    feat: add new interface for random value class generator helping with mock testing

---
+++ HW01/Expedition178/Interfaces/IRandomGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expedition178.Interfaces
+{
+    public interface IRandomGenerator
+    {
+        int GetNext(int min, int max);
+    }
+}
