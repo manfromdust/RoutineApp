fe77116f1ee57177c898b3210bdfae76b61f7beb
Filip Kudera <xkudera@fi.muni.cz>
2026-03-07 23:13:28 +0100

    feat: implement RandomGenerator class which will be used in all RNGs except testing

---
+++ HW01/Expedition178/GameMechanics/RandomGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Expedition178.Interfaces;
+
+namespace Expedition178.GameMechanics
+{
+    public class RandomGenerator : IRandomGenerator
+    {
+        public int GetNext(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1);
+        }
+    }
+}
