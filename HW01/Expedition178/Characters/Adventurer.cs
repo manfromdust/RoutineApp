68d228b75527712793c42e8e785ab48bda32d10e
Filip Kudera <xkudera@fi.muni.cz>
2026-03-07 18:00:23 +0100

    feat: implement methods for gaining experience and lvling up

--- HW01/Expedition178/Characters/Adventurer.cs
+++ HW01/Expedition178/Characters/Adventurer.cs
@@ -13,6 +13,23 @@ namespace Expedition178.Characters
 
         public Adventurer(string name, CharacterType charType, AttackType attackType) : base(name, charType, attackType) { }
 
+        private void CheckLevelUp()
+        {
+            if (experience >= 100)
+            {
+                level++;
+                experience -= 100;
+                Console.WriteLine($"{name} leveled up to level {level}!");
+            }
+        }
+
+        public void GainExperience()
+        {
+            Random random = new Random();
+            experience += random.Next(40, 81);
+            CheckLevelUp();
+        }
+
         public override string ToString()
         {
             return $"{base.ToString()}, Level: {level}, Experience {experience}.";
