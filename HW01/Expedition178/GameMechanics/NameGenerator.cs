d6e2633078a090b8e90bea190a8567b40bcad2d7
Filip Kudera <xkudera@fi.muni.cz>
2026-03-07 19:24:48 +0100

    fix: endless loop

--- HW01/Expedition178/GameMechanics/NameGenerator.cs
+++ HW01/Expedition178/GameMechanics/NameGenerator.cs
@@ -16,13 +16,14 @@ namespace Expedition178.GameMechanics
             var indexes = new HashSet<int>();
             string[] result = new string[count];
 
+
             for (int i = 0; i < count; i++)
             {
                 int index;
                 do
                 {
                     index = random.Next(names.Length);
-                } while (!indexes.Contains(index));
+                } while (indexes.Contains(index));
                 
                 indexes.Add(index);
                 result[i] = names[index];
