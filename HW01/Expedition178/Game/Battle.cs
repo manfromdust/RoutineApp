1850e7248f7868279a3645821a114476b8fa091c
Filip Kudera <xkudera@fi.muni.cz>
2026-03-07 18:11:49 +0100

    feat: implement PerformBattle battle

--- HW01/Expedition178/Game/Battle.cs
+++ HW01/Expedition178/Game/Battle.cs
@@ -1,5 +1,6 @@
 ﻿using Expedition178.Characters;
 using Expedition178.GameMechanics;
+using Expedition178.Game.Parameters;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,7 +23,34 @@ namespace Expedition178.Game
 
         public Character[] PerformBattle(Adventurer[] player, Monster[] enemy)
         {
-            
+            int playerIndex = 0;
+            int enemyIndex = 0;
+
+            while (playerIndex < player.Length && enemyIndex < enemy.Length)
+            {
+                var winner = PerformRound(player[playerIndex], enemy[enemyIndex]);
+
+                if (winner is Adventurer)
+                {
+                    if (!enemy[enemyIndex].IsAlive())
+                    {
+                        enemyIndex++;
+                    }
+                }
+                else
+                {
+                    playerIndex++;
+                }
+            }
+
+            int experienceMultiplier = (playerIndex < enemyIndex) ? Parameters.Parameters.WonMultiplier : 1;
+
+            foreach (var adventurer in player)
+            {
+                adventurer.GainExperience(experienceMultiplier);
+            }
+
+            return (playerIndex < enemyIndex) ? player : enemy;
         }
 
         public Character PerformRound(Adventurer adventurer, Monster creature)
