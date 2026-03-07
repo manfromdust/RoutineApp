efe881a39aa756d807845fceefb580005d2bb3ab
Filip Kudera <xkudera@fi.muni.cz>
2026-03-07 17:28:23 +0100

    refactor: change attack field to public property with protected setter

--- HW01/Expedition178/Characters/Character.cs
+++ HW01/Expedition178/Characters/Character.cs
@@ -10,7 +10,7 @@ namespace Expedition178.Characters
     public abstract class Character
     {
         public readonly string name;
-        protected int attack;
+        public int Attack { get; protected set; }
         protected int life;
         protected int speed;
         protected readonly CharacterType charType;
@@ -27,21 +27,23 @@ namespace Expedition178.Characters
             switch (charType)
             {
                 case CharacterType.Tank:
-                    attack = random.Next(Parameters.MinTankAttack, Parameters.MaxTankAttack + 1);
+                    Attack = random.Next(Parameters.MinTankAttack, Parameters.MaxTankAttack + 1);
                     life = random.Next(Parameters.MinTankLife, Parameters.MaxTankLife + 1);
                     speed = random.Next(Parameters.MinTankSpeed, Parameters.MaxTankSpeed + 1);
                     break;
                 case CharacterType.Dps:
-                    attack = random.Next(Parameters.MinDpsAttack, Parameters.MaxDpsAttack + 1);
+                    Attack = random.Next(Parameters.MinDpsAttack, Parameters.MaxDpsAttack + 1);
                     life = random.Next(Parameters.MinDpsLife, Parameters.MaxDpsLife + 1);
                     speed = random.Next(Parameters.MinDpsSpeed, Parameters.MaxDpsSpeed + 1);
                     break;
             }
         }
 
+
+
         public override string ToString()
         {
-            return $"{name} ({charType}, {attackType}) - Attack: {attack}, Life: {life}, Speed: {speed}";
+            return $"{name} ({charType}, {attackType}) - Attack: {Attack}, Life: {life}, Speed: {speed}";
         }
     }
 }
