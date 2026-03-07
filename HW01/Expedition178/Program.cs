09fbeafd9cd2f2ea731a4aeb720b280d80e67cb3
Filip Kudera <xkudera@fi.muni.cz>
2026-03-07 18:55:36 +0100

    feat: add loop menu so user can decide if play again or quit program

--- HW01/Expedition178/Program.cs
+++ HW01/Expedition178/Program.cs
@@ -6,8 +6,35 @@ namespace Expedition178
     {
         static void Main(string[] args)
         {
-            Game.Game game = new Game.Game();
-            game.Start();
+            Console.WriteLine("Welcome to Expedition 178!");
+            while (true)
+            {
+                Console.WriteLine("Type 'start' to begin a new game or 'quit' to exit.");
+                Console.Write("Your choice: ");
+
+                string? input = Console.ReadLine();
+
+                if (input == null)
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                    continue;
+                }
+
+                switch (input)
+                {
+                    case "start":
+                        Game.Game game = new Game.Game();
+                        game.Start();
+                        break;
+                    case "quit":
+                        Console.WriteLine("Thanks for playing Expedition 178!");
+                        return;
+                    default:
+                        Console.WriteLine("Unknown command. Please type 'start' or 'quit'.");
+                        break;
+                }
+            }
+            
         }
     }
 }
