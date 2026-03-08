b56e10a698f32672734c5f0f26a78e1471549096
Filip Kudera <xkudera@fi.muni.cz>
2026-03-08 19:52:09 +0100

    fix: lower the values 10x

--- HW01/Expedition178/GameMechanics/DmgModifiers.cs
+++ HW01/Expedition178/GameMechanics/DmgModifiers.cs
@@ -7,8 +7,8 @@ namespace Expedition178.GameMechanics
     public enum DmgModifiers
     {
         Immunity = 0,
-        None = 100,
-        Resistance = 50,
-        Weakness = 150
+        None = 10,
+        Resistance = 5,
+        Weakness = 15
     }
 }
