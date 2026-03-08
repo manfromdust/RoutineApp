c639c1f499b72d48ecbe6db3338f10eca0301d70
Filip Kudera <xkudera@fi.muni.cz>
2026-03-08 19:44:20 +0100

    feat: create enum class for damage modifiers in monsters

---
+++ HW01/Expedition178/GameMechanics/DmgModifiers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expedition178.GameMechanics
+{
+    public enum DmgModifiers
+    {
+        Immunity = 0,
+        None = 100,
+        Resistance = 50,
+        Weakness = 150
+    }
+}
