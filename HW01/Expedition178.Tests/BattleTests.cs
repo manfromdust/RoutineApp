82f199168bf25c258b3d11812f11cfd445c1f3e0
Filip Kudera <xkudera@fi.muni.cz>
2026-03-09 18:22:34 +0100

    test: add more tests to test PerformBattle method

--- HW01/Expedition178.Tests/BattleTests.cs
+++ HW01/Expedition178.Tests/BattleTests.cs
@@ -69,5 +69,60 @@ namespace Expedition178.Tests
             Character? expected = null;
             Assert.Equal(expected, battle.PerformRound(adventurer, monster));
         }
+
+        [Fact]
+        public void OnePlayerAgainstMultipleMonstersOfEqualStats()
+        {
+            var random = new FakeRandomGenerator();
+            var battle = new Battle(1, random);
+            var adventurer = new Adventurer("Test01", CharacterType.Dps, AttackType.Fire, random);
+            var monster = new Monster("Test02", CharacterType.Dps, AttackType.Dark, MonsterType.Radiant, 1, random);
+            var monster2 = new Monster("Test03", CharacterType.Dps, AttackType.Dark, MonsterType.Radiant, 1, random);
+            var monster3 = new Monster("Test03", CharacterType.Tank, AttackType.Dark, MonsterType.Radiant, 1, random);
+            Monster[] monsters = [monster, monster2, monster3];
+            Adventurer[] adventurers = [adventurer];
+
+            var expected = adventurers;
+
+            Assert.Equal(expected, battle.PerformBattle(adventurers, monsters));
+        }
+
+        [Fact]
+        public void NormalGameEqualStatsPlayerWins()
+        {
+            var random = new FakeRandomGenerator();
+            var battle = new Battle(1, random);
+            Adventurer[] adventurers = [new Adventurer("Test01", CharacterType.Dps, AttackType.Fire, random),
+                new Adventurer("Test02", CharacterType.Dps, AttackType.Light, random),
+                new Adventurer("Test03", CharacterType.Tank, AttackType.Frost, random)
+            ];
+            Monster[] monsters = [new Monster("Test04", CharacterType.Dps, AttackType.Dark, MonsterType.Natural, 1, random),
+                new Monster("Test05", CharacterType.Dps, AttackType.Dark, MonsterType.Shadow, 1, random),
+                new Monster("Test06", CharacterType.Tank, AttackType.Dark, MonsterType.Natural, 1, random)
+            ];
+
+            var expected = adventurers;
+
+            Assert.Equal(expected, battle.PerformBattle(adventurers, monsters));
+        }
+
+        [Fact]
+        public void NormalGameButImmuneMonstersMonstersWins()
+        {
+            var random = new FakeRandomGenerator();
+            var battle = new Battle(1, random);
+            Adventurer[] adventurers = [new Adventurer("Test01", CharacterType.Dps, AttackType.Light, random),
+                new Adventurer("Test02", CharacterType.Dps, AttackType.Light, random),
+                new Adventurer("Test03", CharacterType.Tank, AttackType.Dark, random)
+            ];
+            Monster[] monsters = [new Monster("Test04", CharacterType.Dps, AttackType.Dark, MonsterType.Radiant, 1, random),
+                new Monster("Test05", CharacterType.Dps, AttackType.Dark, MonsterType.Radiant, 1, random),
+                new Monster("Test06", CharacterType.Tank, AttackType.Dark, MonsterType.Shadow, 1, random)
+            ];
+
+            var expected = monsters;
+
+            Assert.Equal(expected, battle.PerformBattle(adventurers, monsters));
+        }
     }
 }
