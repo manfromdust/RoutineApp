7975bac6f2ceed058b8a6c381ca5495327697d16
Filip Kudera <xkudera@fi.muni.cz>
2026-03-29 15:07:29 +0200

    feat: add folder structures for MVVM

---
+++ HW02/CrossStitching/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CrossStitching
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HW02/CrossStitching/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CrossStitching
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
