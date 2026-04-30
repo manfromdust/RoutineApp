362786230416e1a0acd3952ca6e11b3a93d725e6
Filip Kudera <xkudera@fi.muni.cz>
2026-04-30 19:43:39 +0200

    feat: add quote model linked to routine id

---
+++ Project/RoutineApp/Models/RoutineQuote.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace RoutineApp.Models
+{
+    public class RoutineQuote
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Indexed]
+        public int RoutineId { get; set; }
+        public string Quote { get; set; } = string.Empty;
+    }
+}
