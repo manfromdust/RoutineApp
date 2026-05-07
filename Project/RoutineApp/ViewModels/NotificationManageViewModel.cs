bf060a62c3a440309220575bc4770e8c1226284c
Filip Kudera <xkudera@fi.muni.cz>
2026-05-07 18:19:02 +0200

    refactor: adapt method to notification system

--- Project/RoutineApp/ViewModels/NotificationManageViewModel.cs
+++ Project/RoutineApp/ViewModels/NotificationManageViewModel.cs
@@ -4,16 +4,19 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RoutineApp.Models;
 using RoutineApp.Repositories;
+using RoutineApp.Services;
 using System.Collections.ObjectModel;
 
 namespace RoutineApp.ViewModels
 {
     [QueryProperty(nameof(RoutineId), "RoutineId")]
     [QueryProperty(nameof(NotificationRepo), "NotificationRepo")]
+    [QueryProperty(nameof(QuoteRepo), "QuoteRepo")]
     public partial class NotificationManageViewModel : ObservableObject
     {
         private int _routineId;
         private INotificationRepository _notificationRepo;
+        private IQuoteItemRepository _quoteItemRepository;
 
         public int RoutineId
         {
@@ -27,6 +30,12 @@ namespace RoutineApp.ViewModels
             set => SetProperty(ref _notificationRepo, value);
         }
 
+        public IQuoteItemRepository QuoteRepo
+        {
+            get => _quoteItemRepository;
+            set => SetProperty(ref _quoteItemRepository, value);
+        }
+
         [ObservableProperty]
         ObservableCollection<NotificationRecordViewModel> notifications = new();
 
@@ -34,6 +43,7 @@ namespace RoutineApp.ViewModels
         TimeSpan notificationToAdd;
 
         [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(ActiveButtonText))]
         NotificationRecordViewModel selectedNotification;
 
         public NotificationManageViewModel()
@@ -47,6 +57,8 @@ namespace RoutineApp.ViewModels
 
             Task.Run(async () => await LoadNotificationsAsync());
         }
+
+        public string ActiveButtonText => SelectedNotification != null && SelectedNotification.Notification.Active ? "Deactivate" : "Activate";
 
         private async Task LoadNotificationsAsync()
         {
@@ -66,16 +78,27 @@ namespace RoutineApp.ViewModels
         [RelayCommand]
         public async Task ChangeActiveAsync()
         {
-                if (SelectedNotification == null)
-                {
-                    var toast = Toast.Make("Please select a notification to change.", ToastDuration.Short, 14);
-                    await toast.Show();
-                    return;
-                }
-                SelectedNotification.Notification.Active = !SelectedNotification.Notification.Active;
-                await NotificationRepo.UpdateItemAsync(SelectedNotification.Notification);
-                var toastSuccess = Toast.Make("Notification updated", ToastDuration.Short, 14);
-                await toastSuccess.Show();
+            if (SelectedNotification == null)
+            {
+                var toast = Toast.Make("Please select a notification to change.", ToastDuration.Short, 14);
+                await toast.Show();
+                return;
+            }
+            if (SelectedNotification.Notification.Active)
+            {
+                NotificationService.CancelNotifications(SelectedNotification.Notification.Id);
+            } else
+            {
+                var randomQuotes = await QuoteRepo.GetRandomQuotes(RoutineId, 30);
+                await NotificationService.ScheduleDailyQuotesAsync(SelectedNotification.Notification.Id,
+                                                                   "Your Routine",
+                                                                   SelectedNotification.Notification.TimeOfDay,
+                                                                   randomQuotes);
+            }
+            SelectedNotification.Notification.Active = !SelectedNotification.Notification.Active;
+            await NotificationRepo.UpdateItemAsync(SelectedNotification.Notification);
+            var toastSuccess = Toast.Make("Notification updated", ToastDuration.Short, 14);
+            await toastSuccess.Show();
         }
 
         [RelayCommand]
